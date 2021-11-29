41d340bae6142feb8e9228c45877110e7468b019
F-Scienza <cufa1905@gmail.com>
2021-11-29 11:27:46 -0300

    consigna en readme

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-//  Realizar un proyecto de servidor basado en node.js 
-//  que utilice el módulo express e implemente los siguientes
-//  endpoints en el puerto 8080:
-//      Ruta get '/productos' que devuelva un array con 
-//      todos los productos disponibles en el servidor
-//      Ruta get '/productoRandom' que devuelva un producto 
-//      elegido al azar entre todos los productos disponibles
-//  Incluir un archivo de texto 'productos.txt' 
-//  y utilizar la clase Contenedor del desafío anterior para
-//  acceder a los datos persistidos del servidor.
 const express = require('express')
 const Contenedor = require('./contenedor')
 
